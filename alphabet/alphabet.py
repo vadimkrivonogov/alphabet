272829038643d3cf99954499e8433d2fbd028d2a
vadimkrivonogov <120089193+vadimkrivonogov@users.noreply.github.com>
2023-03-14 15:04:19 +0200

    Update alphabet.py

--- alphabet/alphabet.py
+++ alphabet/alphabet.py
@@ -34,3 +34,82 @@ while name_number > 9:
 
 # Вывод результата
 print(f"Число вашего имени: {name_number}")
+
+
+from tkinter import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+# функция для решения квадратного уравнения
+def solve_quadratic():
+    a = float(a_entry.get())
+    b = float(b_entry.get())
+    c = float(c_entry.get())
+    
+    if a == 0:
+        result_label.config(text="Ошибка: a не должно быть равно нулю", fg="red")
+        return
+    
+    D = b*b - 4*a*c
+    
+    if D < 0:
+        result_label.config(text="Нет действительных корней", fg="red")
+        return
+    elif D == 0:
+        x = -b / (2*a)
+        result_label.config(text=f"Уравнение имеет один корень: x={x}", fg="green")
+    else:
+        x1 = (-b + np.sqrt(D)) / (2*a)
+        x2 = (-b - np.sqrt(D)) / (2*a)
+        result_label.config(text=f"Корни уравнения: x1={x1}, x2={x2}", fg="green")
+
+# функция для построения графика квадратичной функции
+def plot_graph():
+    a = float(a_entry.get())
+    b = float(b_entry.get())
+    c = float(c_entry.get())
+    
+    x = np.linspace(-10, 10, 1000)
+    y = a*x*x + b*x + c
+    
+    plt.plot(x, y)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Квадратичная функция')
+    plt.grid(True)
+    plt.show()
+
+# создание окна
+root = Tk()
+root.title("Решение квадратного уравнения")
+
+# создание элементов интерфейса
+a_label = Label(root, text="a")
+a_label.grid(row=0, column=0)
+
+a_entry = Entry(root)
+a_entry.grid(row=0, column=1)
+
+b_label = Label(root, text="b")
+b_label.grid(row=1, column=0)
+
+b_entry = Entry(root)
+b_entry.grid(row=1, column=1)
+
+c_label = Label(root, text="c")
+c_label.grid(row=2, column=0)
+
+c_entry = Entry(root)
+c_entry.grid(row=2, column=1)
+
+solve_button = Button(root, text="Решить", command=solve_quadratic)
+solve_button.grid(row=3, column=0)
+
+plot_button = Button(root, text="Построить график", command=plot_graph)
+plot_button.grid(row=3, column=1)
+
+result_label = Label(root, text="")
+result_label.grid(row=4, column=0, columnspan=2)
+
+# запуск окна
+root.mainloop()
